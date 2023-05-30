027b875e4c7972950ce38ca19fe137219ebc2462
clem.png <clem.netange@gmail.com>
2023-05-30 16:46:31 +0200

    initialisation

---
+++ zeldiablo/src/gameLaby/laby/LabyJeu.java
@@ -0,0 +1,50 @@
+package gameLaby.laby;
+
+import moteurJeu.Clavier;
+import moteurJeu.Jeu;
+
+import java.io.IOException;
+
+public class LabyJeu implements Jeu {
+
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+
+    private final Labyrinthe laby ;
+
+    public LabyJeu () throws IOException {
+        this.laby = new Labyrinthe("zeldiablo/labySimple/laby1.txt") ;
+    }
+
+    @Override
+    public void update(double secondes, Clavier clavier) {
+        if (clavier.haut){
+            laby.deplacerPerso("HAUT");
+        }
+
+        if (clavier.bas){
+            laby.deplacerPerso("BAS");
+        }
+
+        if (clavier.gauche){
+            laby.deplacerPerso("GAUCHE");
+        }
+
+        if (clavier.droite){
+            laby.deplacerPerso("DROITE");
+        }
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public boolean etreFini() {
+        return false;
+    }
+
+    public Labyrinthe getLaby() { return this.laby ;}
+    
+}
