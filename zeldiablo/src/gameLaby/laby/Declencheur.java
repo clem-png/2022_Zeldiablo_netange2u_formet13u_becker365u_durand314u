0d14ceebae187e690b95648b419e29279e362e36
Quentin <quentin.durand7@etu.univ-lorraine.fr>
2023-06-01 13:19:15 +0200

    Ajout classe Declencheur

---
+++ zeldiablo/src/gameLaby/laby/Declencheur.java
@@ -0,0 +1,42 @@
+package gameLaby.laby;
+
+public class Declencheur {
+    private int x, y;
+    private boolean active;
+
+    public Declencheur (int x, int y, boolean act){
+        this.x = x ;
+        this.y = y ;
+        this.active = false;
+    }
+
+    public boolean etrePresent(int dx, int dy) {
+
+        return (this.x == dx && this.y == dy);
+    }
+
+    /**
+     * @return position x du déclencheur
+     */
+    public int getX() {
+        // getter
+        return this.x;
+    }
+
+    /**
+     * @return position y du déclencheur
+     */
+    public int getY() {
+        //getter
+        return this.y;
+    }
+
+    /**
+     * @return état du déclencheur
+     */
+    public boolean getActive(){
+        //getter
+        return active;
+    }
+
+}
